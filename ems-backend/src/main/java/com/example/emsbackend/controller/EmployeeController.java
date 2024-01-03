90beec573cd6cde64a5ffd92e4541f33ab4d6a6f
MassimilianoVisintainer <61659612+MassimilianoVisintainer@users.noreply.github.com>
2024-01-03 16:08:27 +0000

    Adding GETBYID and GETALL Rest Api

--- ems-backend/src/main/java/com/example/emsbackend/controller/EmployeeController.java
+++ ems-backend/src/main/java/com/example/emsbackend/controller/EmployeeController.java
@@ -6,10 +6,9 @@ import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -18,10 +17,22 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
-    @PostMapping("/create")
+    @PostMapping()
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
 
         EmployeeDto createdEmployee = employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(createdEmployee, HttpStatus.CREATED);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long id) {
+        EmployeeDto employeeDto = employeeService.getEmployeeById(id);
+        return ResponseEntity.ok(employeeDto);
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<EmployeeDto>> getAllEmployees() {
+        List<EmployeeDto> employeeDtos = employeeService.getAllEmployees();
+        return  ResponseEntity.ok(employeeDtos);
     }
 }
--- ems-backend/src/main/java/com/example/emsbackend/service/EmployeeService.java
+++ ems-backend/src/main/java/com/example/emsbackend/service/EmployeeService.java
@@ -1,11 +1,15 @@
 package com.example.emsbackend.service;
 
 import com.example.emsbackend.dto.EmployeeDto;
+import com.example.emsbackend.exception.ResourceNotFoundException;
 import com.example.emsbackend.mapper.EmployeeMapper;
 import com.example.emsbackend.model.Employee;
 import com.example.emsbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -21,4 +25,26 @@ public class EmployeeService implements IEmployeeService {
         return EmployeeMapper.mapToEmployeeDto(employeeCreated);
 
     }
+
+    @Override
+    public EmployeeDto getEmployeeById( Long id){
+        Employee employee = employeeRepository.getReferenceById(id);
+
+        if (employee == null) {
+            throw new ResourceNotFoundException("Employee is not existing with the following id :" + id);
+        }
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees(){
+
+        List<Employee> employees = employeeRepository.findAll();
+
+
+        List<EmployeeDto> employeeDtos = employees.stream().map((employee -> EmployeeMapper.mapToEmployeeDto(employee))).collect(Collectors.toList());
+
+        return employeeDtos;
+    }
+
 }
